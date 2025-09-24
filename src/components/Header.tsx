94bc8c35a636e6043423ddccb6b1d84a8f18f7bb
Dorukzengin <dorukzengin57@gmail.com>
2025-09-24 22:19:26 +0300

    Fix Logo Size Issues

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ export const Header: React.FC<HeaderProps> = ({
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex items-center space-x-3">
-            <div className="w-8 h-8 flex items-center justify-center">
+            <div className="w-10 h-10 flex items-center justify-center">
               <img 
                 src="/logo.png" 
                 alt="NexToImage Logo" 
